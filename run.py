e34f0b138dbe84656f1b1732bb4ea2d9972a2ac4
Roman Vykuka <f.jednicka@seznam.cz>
2012-07-15 22:53:49 +0200

    Added support for hosts file editing

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 """
 Spyder Editor
 """
-import os, sys
+import os, sys, re
 
 def sinput(text):
     text = raw_input(text)    
@@ -35,10 +35,24 @@ if os.path.exists(projectDir + "/index.html") == False:
     try:
         hfile = open(projectDir + "/index.html", "w")
     except IOError:
-        print('Cannot create index.html file!')
+        print("Cannot create index.html file!")
     else:
-        hfile.write("<html>\n<head>\n<title>" + projectName +"</title>\n</head>\n<body>\n<h1>Hello " + projectName + "</h1>\n</body>\n</html>\n")
+        hfile.write("<html>\n<head>\n<title>" + projectName + "</title>\n</head>\n<body>\n<h1>Hello " + projectName + "</h1>\n</body>\n</html>\n")
         hfile.close() 
+try:        
+    hosts = open("/etc/hosts", "r")
+except IOError:
+    print("Cannot open /etc/hosts - virtual domain name was not created!")
+else:
+    domainExists = re.search(r"(127.0.[0|1].1)(\s+)(www.)?(" + projectDomain + ")", hosts.read(), re.MULTILINE | re.IGNORECASE)
+    hosts.close()
+    
+    if domainExists == None:
+        if os.access("/etc/hosts", os.W_OK):
+            os.system('sed -i "1i127.0.0.1\t' + projectDomain + '\\n127.0.0.1\twww.' + projectDomain + '\\n" /etc/hosts')
+        else:
+            print('You are not allowed to write to hosts file. Please login as root in following dialog!')
+            os.system('sudo sed -i "1i127.0.0.1\t' + projectDomain + '\\n127.0.0.1\twww.' + projectDomain + '\\n" /etc/hosts')
 
 if('git' in options or 'git-ftp' in options):    
     if os.path.exists(projectDir + "/.git"):
@@ -57,7 +71,7 @@ if('git-ftp' in options):
         print('Cannot open project git configuration file!')
     else:
         hfile.write("[git-ftp]\n\tuser = " + gFtpUser + "\n\tpassword = " + gFtpPass+ "\n\turl = " + gFtpLoc + "\n")
-        hfile.close()    
+        hfile.close()   
     
 os.system("firefox " + projectDomain)
 
